e3fc4f74e99973077397bf0f318a5ea42acc40e1
Rayhan <saykotrayhan199@gmail.com>
2022-08-15 23:53:39 +0600

    finished programs values and faq section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './home.css'
 import MainHeader from '../../components/MainHeader'
 import Programs from '../../components/Programs'
+import Values from '../../components/Values'
+import FAQs from '../../components/FAQs'
 
 const Home = () => {
   return (
     <>
       <MainHeader />
-      <Programs/>
+      <Programs />
+      <Values />
+      <FAQs />
     </>
   )
 }
