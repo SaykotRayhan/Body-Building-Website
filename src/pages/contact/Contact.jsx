7a4af83e4ce55cd5d78a78eccaaf636ba40f3773
Rayhan <saykotrayhan199@gmail.com>
2022-08-22 21:27:40 +0600

    Finished contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import './contact.css'
+import Header from '../../components/Header'
+import HeaderImage from '../../images/header_bg_2.jpg'
+import { MdEmail } from 'react-icons/md'
+import { BsMessenger } from 'react-icons/bs'
+import { IoLogoWhatsapp } from 'react-icons/io'
 
 const Contact = () => {
   return (
-    <div>
-      Contact
-    </div>
+    <>
+      <Header title='Get In Touch' image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+        praesentium facere culpa dolorum voluptatibus ex vitae corrupti
+        assumenda sint id?
+      </Header>
+      <section className='contact'>
+        <div className='container contact__container'>
+          <div className='contact__wrapper'>
+            <a
+              href='mailto:saykotrayhan199@gmail.com'
+              target='_blank'
+              rel='noreffer noopener'
+            >
+              <MdEmail />
+            </a>
+            <a href='#' target='_blank' rel='noreffer noopener'>
+              <BsMessenger />
+            </a>
+            <a href='#' target='_blank' rel='noreffer noopener'>
+              <IoLogoWhatsapp />
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
